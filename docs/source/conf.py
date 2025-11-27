9a26863e7bb64d5b7e6d6af9bfdab2d208020400
***** <721466+soodoku@users.noreply.github.com>
2025-11-27 04:19:12 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,8 +62,8 @@ extensions = [
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-source_suffix = {".md": "myst_parser"}
+# MyST parser will handle .md files
+source_suffix = ['.md']
 
 # The master toctree document.
 master_doc = "index"
